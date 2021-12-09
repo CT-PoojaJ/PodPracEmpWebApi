66669e5f2c9c799e274072b13f6574ff8f1177cc
Pooja <Pooja.Joshi@citiustech.com>
2021-12-09 09:50:20 +0530

    Commiting project to git

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeWebAppPODPrac.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeWebAppPODPrac.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        List<Employee> lstEmp;
+        public EmployeesController()
+        {
+            Employee emp = new Employee() { EmpId = 1, Name = "Zack" };
+            Employee emp1 = new Employee() { EmpId = 2, Name = "Cody" };
+            lstEmp.Add(emp);
+            lstEmp.Add(emp1);
+        }
+        // GET: api/<EmployeesController>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return lstEmp;
+        }
+
+        // GET api/<EmployeesController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            Employee emp = lstEmp.Where(x => x.EmpId == id).FirstOrDefault();
+            return emp;
+        }
+
+        // POST api/<EmployeesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebAppPODPrac.Model
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+    }
+}
